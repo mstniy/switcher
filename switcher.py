bcdf7cd28efa4a9c76480160058ad7059c9d2ef2
Kartal Kaan Bozdoğan <kartalkaanbozdogan@gmail.com>
2020-06-03 18:37:27 +0300

    Awesome

--- switcher.py
+++ switcher.py
@@ -8,50 +8,73 @@ def vcopen(arg):
 	cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
 	return cap
 
+MAX_AD_LENGTH = 60 # Seconds
+ad_frames = []
+ad_idx = 0
+ad_dir = 1
+
+def adRead():
+	global ad_frames
+	global ad_idx
+	global ad_dir
+
+	ad_idx += ad_dir
+	if ad_idx < 0:
+		ad_idx = 0
+		ad_dir = 1
+	if ad_idx >= len(ad_frames):
+		ad_idx = len(ad_frames)-1
+		ad_dir = -1
+	return ad_frames[ad_idx]
+
 def main(argv):
-	adName = argv[0]
-	cap_camera = vcopen(0)
-	cap_camera.set(cv2.CAP_PROP_BUFFERSIZE, 3)
-	cap_camera.grab() # Force start the camera
-	cap_ad = vcopen(adName)
-	ad_fps = cap_ad.get(cv2.CAP_PROP_FPS)
-	ad_last_read = time.time()
-	cap = cap_ad
+	global ad_frames
+	global ad_idx
+	global ad_dir
+
+	cap = vcopen(0)
+	cap.set(cv2.CAP_PROP_BUFFERSIZE, 3)
+	#cap.grab() # Force start the camera
+	show_ad = 0
+	ad_last_read = 0
+	fps = cap.get(cv2.CAP_PROP_FPS)
 	while cap.isOpened():
 
-		if cap is cap_ad:
-			sleep_dur = ad_last_read+1/ad_fps - time.time()
-			if sleep_dur > 0:
-				time.sleep(sleep_dur)
-			ad_last_read = time.time()
-
-		ret, frame = cap.read()
-		#height, width, channels = frame.shape
 		key = cv2.waitKey(33)
 		if key==27:
 			print('Switching...', file=sys.stderr)
-			if cap is cap_camera:
-				cap = cap_ad
+			if show_ad == 0:
+				show_ad = 1
+				ad_idx = len(ad_frames)
+				ad_dir = -1
 			else:
-				cap_camera.grab()
-				cap_camera.grab()
-				cap_camera.grab()
-				cap = cap_camera
+				show_ad = 0
+				ad_frames = []
+				cap.grab() # Flush the buffer
+				cap.grab()
+				cap.grab()
 
-		if(ret):
-			sys.stdout.buffer.write(frame.tostring())
+		if show_ad:
+			sleep_dur = ad_last_read+1/fps - time.time()
+			if sleep_dur > 0:
+				time.sleep(sleep_dur)
+			ad_last_read = time.time()
+			frame = adRead()
+			_, ghost_image = cap.read()
+			display_image = frame//4*3+ghost_image//4
+			cv2.putText(display_image, 'AD', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+			cv2.imshow("frame",display_image)
+
+		if not show_ad:
+			_, frame = cap.read()
+			#height, width, channels = frame.shape
+			ad_frames.append(frame)
+			if len(ad_frames)/fps > MAX_AD_LENGTH:
+				del ad_frames[0]
 			cv2.imshow("frame",frame)
-		else:
-			print('Reopening stream...', file=sys.stderr)
-			cap.release()
-			if cap is cap_camera:
-				cap_camera = vcopen(0)
-				cap_camera.set(cv2.CAP_PROP_BUFFERSIZE, 3)
-				cap_camera.grab() # Force start the camera
-				cap = cap_camera
-			else:
-				cap_ad = vcopen(adName)
-				cap = cap_ad
+
+		sys.stdout.buffer.write(frame.tostring())
+		
 
 	cap.release()
 
