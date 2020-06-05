9c13caee3d967226b564df6af7851860c24c4a8f
Kartal Kaan Bozdoğan <kartalkaanbozdogan@gmail.com>
2020-06-05 14:50:46 +0300

    Added a line to stream over udp

--- switch_stream_video.sh
+++ switch_stream_video.sh
@@ -6,8 +6,8 @@ sudo modprobe v4l2loopback video_nr=2 exclusive_caps=1 card_label="Generic USB2.
 
 while true
 do
-    python3 switcher.py "$1" | ffmpeg -r 10 -f rawvideo -pixel_format bgr24 -video_size 1280x720 -i pipe:0 -metadata major_brand="isom" -metadata minor_version="512" -metadata compatible_brands="isomiso2avc1mp41" -filter:v "format=yuyv422" -f v4l2 /dev/video2
-done
-#python3 switcher.py "$1" | ffmpeg -r 10 -f rawvideo -pixel_format bgr24 -video_size 1280x720 -i pipe:0 -pixel_format yuv420p deneme.mp4
-#python3 switcher.py "$1" | ffplay -f rawvideo -pixel_format bgr24 -video_size 1280x720 -i - 
+    python3 switcher.py "$1" | ffmpeg -r 10 -f rawvideo -pixel_format bgr24 -video_size 1280x720 -i pipe:0 -filter:v "format=yuyv422" -f v4l2 /dev/video2
+    #python3 switcher.py "$1" | ffmpeg -r 10 -f rawvideo -pixel_format bgr24 -video_size 1280x720 -i pipe:0 -b:v 1M -vcodec mpeg4 -f mpegts udp://127.0.0.1:23000
+    #python3 switcher.py "$1" | ffmpeg -r 10 -f rawvideo -pixel_format bgr24 -video_size 1280x720 -i pipe:0 deneme.mp4
+done 
 
