c15a91cdffca2a293e665f7a14c5f37975bdb70b
Kartal Kaan Bozdoğan <kartalkaanbozdogan@gmail.com>
2020-11-23 01:41:34 +0100

    Disable the physical camera for the duration of the operation

--- switch_stream_video.sh
+++ switch_stream_video.sh
@@ -2,17 +2,35 @@
 
 # The command line argument "cam" emulates a camera.
 
+function cleanup {
+    sudo modprobe -r v4l2loopback
+    sudo setfacl -m u:$USERNAME:rw- /dev/video0
+    sudo setfacl -x u:switcher_user /dev/video0
+    xhost -local:switcher_user
+    sudo userdel switcher_user
+}
+
 if [ "$1" = "cam" -o "$1" = "receive" ]
 then
-    trap 'sudo modprobe -r v4l2loopback' EXIT
+    trap 'cleanup' EXIT
+    # Load the virtual camera
     sudo modprobe v4l2loopback video_nr=2 exclusive_caps=1 card_label="Generic USB2.0 Camera"
+    # Block access to the physical camera
+    sudo setfacl -x u:$USERNAME /dev/video0
+    # Create a dummy account that has access to the physical camera
+    sudo useradd -r -s /bin/false switcher_user
+    sudo setfacl -m u:switcher_user:rw- /dev/video0
+    xhost local:switcher_user
 fi
+
+trap exit SIGINT
 
 while true
 do
     if [ "$1" = "cam" ]
     then
-      python3 switcher.py | ffmpeg -re -f rawvideo -pixel_format bgr24 -video_size 640x480 -i pipe:0 -filter:v "format=yuyv422" -f v4l2 /dev/video2
+      # Run switcher.py as switcher_user, because it needs to be able to read from the physical camera
+      sudo -u switcher_user python3 switcher.py | ffmpeg -re -f rawvideo -pixel_format bgr24 -video_size 640x480 -i pipe:0 -filter:v "format=yuyv422" -f v4l2 /dev/video2
     elif [ "$1" = "broadcast" ]
     then
       python3 switcher.py | ffmpeg -re -f rawvideo -pixel_format bgr24 -video_size 640x480 -i pipe:0 -b:v 1M -vcodec mpeg4 -tune zerolatency -f mpegts udp://127.0.0.1:23000
